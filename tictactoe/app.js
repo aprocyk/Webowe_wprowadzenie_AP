4e2a3bda41056ada836fcbe039d342ac69a8de05
aprocyk <48320987+aprocyk@users.noreply.github.com>
2020-03-29 16:37:32 +0200

    Initial commit

---
+++ tictactoe/app.js
@@ -0,0 +1,72 @@
+"use strict";
+var States;
+(function (States) {
+    States[States["none"] = 0] = "none";
+    States[States["X"] = 1] = "X";
+    States[States["O"] = 2] = "O";
+})(States || (States = {}));
+var Board = /** @class */ (function () {
+    function Board() {
+        this.isP1Winner = false;
+        this.isP2Winner = false;
+        this.isBoardFull = false;
+        this.playerActive = 1;
+        this.cellArray = [];
+    }
+    Board.prototype.initializeCells = function () {
+        var array = [];
+        for (var i = 1; i <= 3; i++) {
+            var row = [];
+            for (var j = 1; j <= 3; j++) {
+                var C = new Cell();
+                C.id = i * 10 + j;
+                row.push(C);
+            }
+            array.push(row);
+        }
+        this.cellArray = array;
+    };
+    Board.prototype.assignCells = function () {
+        this.cellArray.forEach(function (element) {
+            element.forEach(function (cell) {
+                var d = document.getElementById("" + cell.id);
+                var f = function () {
+                    if (b.playerActive === 1) {
+                        if (d)
+                            d.innerText = "X";
+                        console.log("X");
+                        b.playerActive = 2;
+                        d === null || d === void 0 ? void 0 : d.removeEventListener('click', f);
+                        // let pos = cell.id.toString();
+                        // let posy = parseFloat(pos.charAt(0));
+                        // let posx = parseFloat(pos.charAt(1));
+                        // b.cellArray[posy - 1].splice(posx - 1, 1);
+                    }
+                    else if (b.playerActive === 2) {
+                        if (d)
+                            d.innerText = "O";
+                        console.log("O");
+                        b.playerActive = 1;
+                        d === null || d === void 0 ? void 0 : d.removeEventListener('click', f);
+                        // let pos = cell.id.toString();
+                        // let posy = parseFloat(pos.charAt(0));
+                        // let posx = parseFloat(pos.charAt(1));
+                        // b.cellArray[posy - 1].splice(posx - 1, 1);
+                    }
+                };
+                d === null || d === void 0 ? void 0 : d.addEventListener('click', f);
+            });
+        });
+    };
+    return Board;
+}());
+var Cell = /** @class */ (function () {
+    function Cell() {
+        this.state = States.none;
+        this.id = 0;
+    }
+    return Cell;
+}());
+var b = new Board();
+b.initializeCells();
+b.assignCells();
---
+++ tictactoe/app.ts
@@ -0,0 +1,87 @@
+enum States {
+    none,
+    X,
+    O
+}
+
+class Board {
+    isP1Winner: boolean;
+    isP2Winner: boolean;
+    isBoardFull: boolean;
+    playerActive: number;
+    cellArray: Cell[][];
+
+    constructor() {
+        this.isP1Winner = false;
+        this.isP2Winner = false;
+        this.isBoardFull = false;
+        this.playerActive = 1;
+        this.cellArray = [];
+    }
+
+    initializeCells() {
+        let array = [];
+        for (let i = 1; i <= 3; i++) {
+            let row = [];
+            for (let j = 1; j <= 3; j++) {
+                let C = new Cell();
+                C.id = i * 10 + j;
+                row.push(C);
+            }
+            array.push(row);
+        }
+        this.cellArray = array;
+    }
+    assignCells() {
+
+        this.cellArray.forEach(element => {
+            element.forEach(cell => {
+                const d = document.getElementById("" + cell.id);
+                let f = function () {
+                    if (b.playerActive === 1) {
+                        if (d) d.innerText = "X";
+                        console.log("X");
+                        b.playerActive = 2;
+                        d?.removeEventListener('click', f);
+
+                        // let pos = cell.id.toString();
+                        // let posy = parseFloat(pos.charAt(0));
+                        // let posx = parseFloat(pos.charAt(1));
+                        // b.cellArray[posy - 1].splice(posx - 1, 1);
+
+                    } else if (b.playerActive === 2) {
+                        if (d) d.innerText = "O";
+                        console.log("O");
+                        b.playerActive = 1;
+                        d?.removeEventListener('click', f);
+                        // let pos = cell.id.toString();
+                        // let posy = parseFloat(pos.charAt(0));
+                        // let posx = parseFloat(pos.charAt(1));
+                        // b.cellArray[posy - 1].splice(posx - 1, 1);
+
+                    }
+
+                }
+                d?.addEventListener('click', f);
+
+
+
+            });
+        });
+
+    }
+
+}
+
+class Cell {
+    state: States;
+    id: number;
+    constructor() {
+        this.state = States.none;
+        this.id = 0;
+    }
+
+}
+let b = new Board();
+b.initializeCells();
+b.assignCells();
