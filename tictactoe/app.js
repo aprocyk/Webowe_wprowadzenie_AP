fe084c5e06dff81588e1a6b5657204026c99c49d
aprocyk <48320987+aprocyk@users.noreply.github.com>
2020-03-30 15:29:48 +0200

    TicTacToe v0.99
    Game is playable.

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -1,17 +1,18 @@
 "use strict";
 var States;
 (function (States) {
-    States[States["none"] = 0] = "none";
-    States[States["X"] = 1] = "X";
-    States[States["O"] = 2] = "O";
+    States["none"] = "";
+    States["X"] = "X";
+    States["O"] = "O";
 })(States || (States = {}));
 var Board = /** @class */ (function () {
     function Board() {
         this.isP1Winner = false;
         this.isP2Winner = false;
-        this.isBoardFull = false;
+        this.isTie = false;
         this.playerActive = 1;
         this.cellArray = [];
+        this.turn = 0;
     }
     Board.prototype.initializeCells = function () {
         var array = [];
@@ -34,31 +35,143 @@ var Board = /** @class */ (function () {
                     if (b.playerActive === 1) {
                         if (d)
                             d.innerText = "X";
-                        console.log("X");
                         b.playerActive = 2;
                         d === null || d === void 0 ? void 0 : d.removeEventListener('click', f);
                         var pos = cell.id.toString();
                         var posy = parseFloat(pos.charAt(0));
                         var posx = parseFloat(pos.charAt(1));
                         b.cellArray[posy - 1][posx - 1].state = States.X;
-                        // b.cellArray[posy - 1].splice(posx - 1, 1);
+                        b.checkWinHorizontally();
+                        b.checkWinVertically();
+                        b.checkWinDiagonally();
+                        b.checkTie();
                     }
                     else if (b.playerActive === 2) {
                         if (d)
                             d.innerText = "O";
-                        console.log("O");
                         b.playerActive = 1;
                         d === null || d === void 0 ? void 0 : d.removeEventListener('click', f);
                         var pos = cell.id.toString();
                         var posy = parseFloat(pos.charAt(0));
                         var posx = parseFloat(pos.charAt(1));
                         b.cellArray[posy - 1][posx - 1].state = States.O;
-                        // b.cellArray[posy - 1].splice(posx - 1, 1);
+                        b.checkWinHorizontally();
+                        b.checkWinVertically();
+                        b.checkWinDiagonally();
+                        b.checkTie();
                     }
                 };
                 d === null || d === void 0 ? void 0 : d.addEventListener('click', f);
             });
         });
+    };
+    Board.prototype.gameFinished = function () {
+        var d = document.querySelector("#winner");
+        var t = document.querySelector("table");
+        if (t)
+            t.style.pointerEvents = "none";
+        if (this.isP1Winner === true) {
+            if (d)
+                d.innerHTML = "P1 WINS";
+        }
+        else if (this.isP2Winner === true) {
+            if (d)
+                d.innerHTML = "P2 WINS";
+        }
+        else if (this.isTie === true) {
+            if (d)
+                d.innerHTML = "TIE";
+        }
+    };
+    Board.prototype.turnCounter = function () {
+        this.turn++;
+        var d = document.querySelector('#counter');
+        if (d)
+            d.innerHTML = this.turn.toString();
+    };
+    Board.prototype.checkTie = function () {
+        this.turnCounter();
+        if (this.turn === 9 &&
+            this.isP1Winner === false &&
+            this.isP2Winner === false) {
+            this.isTie = true;
+            console.log("TIE");
+            this.gameFinished();
+        }
+    };
+    Board.prototype.checkWinDiagonally = function () {
+        if (this.cellArray[0][0].state === States.X &&
+            this.cellArray[1][1].state === States.X &&
+            this.cellArray[2][2].state === States.X) {
+            this.isP1Winner = true;
+            console.log("P1 WIN!");
+            this.gameFinished();
+        }
+        else if (this.cellArray[0][2].state === States.X &&
+            this.cellArray[1][1].state === States.X &&
+            this.cellArray[2][0].state === States.X) {
+            this.isP1Winner = true;
+            console.log("P1 WIN!");
+            this.gameFinished();
+        }
+        else if (this.cellArray[0][0].state === States.O &&
+            this.cellArray[1][1].state === States.O &&
+            this.cellArray[2][2].state === States.O) {
+            this.isP2Winner = true;
+            console.log("P2 WIN!");
+            this.gameFinished();
+        }
+        else if (this.cellArray[0][2].state === States.O &&
+            this.cellArray[1][1].state === States.O &&
+            this.cellArray[2][0].state === States.O) {
+            this.isP2Winner = true;
+            console.log("P2 WIN!");
+            this.gameFinished();
+        }
+    };
+    Board.prototype.checkWinVertically = function () {
+        var counter = "";
+        for (var i = 0; i <= 2; i++) {
+            for (var j = 0; j < this.cellArray.length; j++) {
+                counter = counter + this.cellArray[j][i].state;
+            }
+            if (counter === "XXX") {
+                console.log("P1 WIN!");
+                this.isP1Winner = true;
+                this.gameFinished();
+                break;
+            }
+            else if (counter === "OOO") {
+                console.log("P2 WIN!");
+                this.isP2Winner = true;
+                this.gameFinished();
+                break;
+            }
+            counter = "";
+        }
+    };
+    Board.prototype.checkWinHorizontally = function () {
+        var counter = "";
+        for (var _i = 0, _a = this.cellArray; _i < _a.length; _i++) {
+            var row = _a[_i];
+            for (var _b = 0, row_1 = row; _b < row_1.length; _b++) {
+                var cell = row_1[_b];
+                counter = counter + cell.state;
+            }
+            if (counter === "XXX") {
+                console.log("P1 WIN!");
+                this.isP1Winner = true;
+                this.gameFinished();
+                break;
+            }
+            else if (counter === "OOO") {
+                console.log("P2 WIN!");
+                this.isP2Winner = true;
+                this.gameFinished();
+                break;
+            }
+            counter = "";
+        }
     };
     return Board;
 }());
--- tictactoe/app.ts
+++ tictactoe/app.ts
@@ -1,75 +1,193 @@
 enum States {
-    none,
-    X,
-    O
+    none = "",
+    X = "X",
+    O = "O"
 }
 
 class Board {
     isP1Winner: boolean;
     isP2Winner: boolean;
-    isBoardFull: boolean;
+    isTie: boolean;
     playerActive: number;
     cellArray: Cell[][];
+    turn: number;
 
     constructor() {
         this.isP1Winner = false;
         this.isP2Winner = false;
-        this.isBoardFull = false;
+        this.isTie = false;
         this.playerActive = 1;
         this.cellArray = [];
+        this.turn = 0;
     }
 
     initializeCells() {
         let array = [];
         for (let i = 1; i <= 3; i++) {
+
             let row = [];
+
             for (let j = 1; j <= 3; j++) {
+
                 let C = new Cell();
                 C.id = i * 10 + j;
                 row.push(C);
+
             }
+
             array.push(row);
         }
         this.cellArray = array;
     }
+
     assignCells() {
 
         this.cellArray.forEach(element => {
+
             element.forEach(cell => {
+
                 const d = document.getElementById("" + cell.id);
+
                 let f = function () {
+
                     if (b.playerActive === 1) {
+
                         if (d) d.innerText = "X";
-                        console.log("X");
                         b.playerActive = 2;
                         d?.removeEventListener('click', f);
-
                         let pos = cell.id.toString();
                         let posy = parseFloat(pos.charAt(0));
                         let posx = parseFloat(pos.charAt(1));
-                        b.cellArray[posy-1][posx-1].state = States.X;
-                        // b.cellArray[posy - 1].splice(posx - 1, 1);
-                        
+                        b.cellArray[posy - 1][posx - 1].state = States.X;
+                        b.checkWinHorizontally();
+                        b.checkWinVertically();
+                        b.checkWinDiagonally();
+                        b.checkTie();
+
                     } else if (b.playerActive === 2) {
+
                         if (d) d.innerText = "O";
-                        console.log("O");
                         b.playerActive = 1;
                         d?.removeEventListener('click', f);
                         let pos = cell.id.toString();
                         let posy = parseFloat(pos.charAt(0));
                         let posx = parseFloat(pos.charAt(1));
-                        b.cellArray[posy-1][posx-1].state = States.O;
-                        // b.cellArray[posy - 1].splice(posx - 1, 1);
-
+                        b.cellArray[posy - 1][posx - 1].state = States.O;
+                        b.checkWinHorizontally();
+                        b.checkWinVertically();
+                        b.checkWinDiagonally();
+                        b.checkTie();
                     }
-
                 }
+
                 d?.addEventListener('click', f);
-
-
-
             });
         });
+
+    }
+
+    gameFinished() {
+        let d = document.querySelector("#winner");
+        let t = document.querySelector("table");
+        if(t) t.style.pointerEvents = "none";
+        if (this.isP1Winner === true) {
+            if(d) d.innerHTML = "P1 WINS";
+        } else if (this.isP2Winner === true) {
+            if(d) d.innerHTML = "P2 WINS";
+        }else if(this.isTie === true){
+            if(d) d.innerHTML = "TIE";
+        }
+    }
+
+    turnCounter() {
+        this.turn++;
+        const d = document.querySelector('#counter');
+        if (d) d.innerHTML = this.turn.toString();
+    }
+    checkTie() {
+        this.turnCounter();
+        if (this.turn === 9 &&
+            this.isP1Winner === false &&
+            this.isP2Winner === false) {
+            this.isTie = true;
+            console.log("TIE");
+            this.gameFinished();
+        }
+    }
+
+    checkWinDiagonally() {
+        if (this.cellArray[0][0].state === States.X &&
+            this.cellArray[1][1].state === States.X &&
+            this.cellArray[2][2].state === States.X) {
+            this.isP1Winner = true;
+            console.log("P1 WIN!");
+            this.gameFinished();
+        } else if (this.cellArray[0][2].state === States.X &&
+            this.cellArray[1][1].state === States.X &&
+            this.cellArray[2][0].state === States.X) {
+            this.isP1Winner = true;
+            console.log("P1 WIN!");
+            this.gameFinished();
+        } else if (this.cellArray[0][0].state === States.O &&
+            this.cellArray[1][1].state === States.O &&
+            this.cellArray[2][2].state === States.O) {
+            this.isP2Winner = true;
+            console.log("P2 WIN!");
+            this.gameFinished();
+        } else if (this.cellArray[0][2].state === States.O &&
+            this.cellArray[1][1].state === States.O &&
+            this.cellArray[2][0].state === States.O) {
+            this.isP2Winner = true;
+            console.log("P2 WIN!");
+            this.gameFinished();
+        }
+    }
+
+    checkWinVertically() {
+        let counter = "";
+        for (let i = 0; i <= 2; i++) {
+            for (let j = 0; j < this.cellArray.length; j++) {
+                counter = counter + this.cellArray[j][i].state;
+
+            }
+
+            if (counter === "XXX") {
+                console.log("P1 WIN!");
+                this.isP1Winner = true;
+                this.gameFinished();
+                break;
+            } else if (counter === "OOO") {
+                console.log("P2 WIN!");
+                this.isP2Winner = true;
+                this.gameFinished();
+                break;
+            }
+            counter = "";
+        }
+
+    }
+
+    checkWinHorizontally() {
+        let counter = "";
+        for (let row of this.cellArray) {
+            for (let cell of row) {
+                counter = counter + cell.state;
+
+            }
+
+            if (counter === "XXX") {
+                console.log("P1 WIN!");
+                this.isP1Winner = true;
+                this.gameFinished();
+                break;
+            } else if (counter === "OOO") {
+                console.log("P2 WIN!");
+                this.isP2Winner = true;
+                this.gameFinished();
+                break;
+            }
+            counter = "";
+        }
 
     }
 
